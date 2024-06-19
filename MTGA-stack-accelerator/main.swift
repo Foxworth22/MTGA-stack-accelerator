d2579268bc33bd85fc9374d3b74ec04b7aa7fa8e
Miles <72813078+Foxworth22@users.noreply.github.com>
2024-06-19 14:44:55 -0600

    Update main.swift
    refactor Stack to hold Cards rather than Strings

--- MTGA-stack-accelerator/main.swift
+++ MTGA-stack-accelerator/main.swift
@@ -7,27 +7,42 @@
 
 import Foundation
 
+struct Card {
+    var action = ""
+}
+
 struct Stack {
-    private var items: [String] = []
+    private var items: [Card] = []
     
-    func peek() -> String {
+    func peek() -> Card {
         guard let topElement = items.first else { fatalError("This stack is empty.") }
         return topElement
     }
     
-    mutating func pop() -> String {
-        return items.removeFirst()
+    mutating func pop() -> Card {
+        var top = items.removeFirst()
+        print(top.action)
+        return top
     }
   
-    mutating func push(_ element: String) {
+    mutating func push(_ element: Card) {
         items.insert(element, at: 0)
     }
 }
 
 var nameStack = Stack()
+var Ace = Card(action: "A")
+var King = Card(action: "K")
+var Queen = Card(action: "Q")
 
-nameStack.push("Caleb")
-nameStack.push("Charles")
-nameStack.push("Tina")
+nameStack.push(Ace)
+nameStack.push(King)
+nameStack.push(Queen)
+
+print(nameStack.pop())
+print(nameStack.pop())
+print(nameStack.pop())
+
+print(nameStack.peek())
 
 print(nameStack)
